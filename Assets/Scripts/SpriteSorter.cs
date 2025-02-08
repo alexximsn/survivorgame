697d5a3f98fb97b4d65e59d651f17b30eedc2db0
alexximsn <280580926@qq.com>
2025-02-08 23:50:33 +0800

    花草贴图和显示

---
+++ Assets/Scripts/SpriteSorter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSorter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spriteRenderer.sortingOrder=-(int)(transform.position.y*10);
+    }
+}
