772756a7bbdd872eb3ffa904181d6f3bdf5b7f8f
alexximsn <280580926@qq.com>
2025-02-12 22:28:44 +0800

    敌人逻辑

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Diagnostics;
+using UnityEngine;
+
+[RequireComponent(typeof(EnemyMovement))]
+public class Enemy : MonoBehaviour
+{
+    private EnemyMovement movement;
+
+    private Player player;
+    [SerializeField] private SpriteRenderer renderer;
+    [SerializeField] private SpriteRenderer spawnIndicator;
+    private bool hasSpawned;
+    [SerializeField] private float playerDetectionRadius;
+    [SerializeField] bool gizmos;
+
+    [SerializeField] private ParticleSystem dieParticles;
+    [SerializeField] private int damage;
+    [SerializeField] private float attackFrequency;
+    private float attackDelay;
+    private float attackTimer;
+    void Start()
+    {
+        movement = GetComponent<EnemyMovement>();
+
+        player = FindObjectOfType<Player>();
+
+        if (player == null)
+        {
+          
+            Destroy(gameObject);
+        }
+        StartSpawnSequence();
+        attackDelay = 1f / attackFrequency;
+
+    }
+    private void StartSpawnSequence()
+    {
+        SetRenderersVisibility(false); 
+        Vector3 targetScale = spawnIndicator.transform.localScale * 1.3f;
+        LeanTween.scale(spawnIndicator.gameObject, targetScale, .3f).
+            setLoopPingPong(4).setOnComplete(SpawnSequenceCompleted);
+
+    }
+    private void SpawnSequenceCompleted()
+    {
+        SetRenderersVisibility(true);
+        hasSpawned = true;
+        movement.StorePlayer(player);
+    }
+   
+    private void SetRenderersVisibility(bool visibility)
+    {
+        renderer.enabled = visibility;
+        spawnIndicator.enabled = !visibility;
+    }
+    void Update()
+    {
+        if (attackTimer >= attackDelay)
+            TryAttack();
+        else
+            Wait();
+    }
+    private void Wait()
+    {
+        attackTimer += Time.deltaTime;
+    }
+    private void Attack()
+    {
+        Debug.Log("伤害" + damage);
+        attackTimer = 0;
+    }
+    private void PassAway()
+    {
+        dieParticles.transform.SetParent(null);
+        dieParticles.Play();
+        Destroy(gameObject);
+    }
+    private void TryAttack()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+        if (distanceToPlayer <= playerDetectionRadius)
+            Attack();
+        //PassAway();
+    }
+    private void OnDrawGizmos()
+    {
+        if (!gizmos)
+            return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, playerDetectionRadius);
+    }
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using System.Threading;
+
+//using System.Diagnostics;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Player player;
+   
+    [SerializeField] private float moveSpeed;
+    
+    void Update()
+    {
+       
+        if(player!=null)
+          FollowPlayer();  
+    }
+    public void StorePlayer(Player player)
+    {
+        this.player = player;
+    }
+    private void FollowPlayer()
+    {
+        Vector2 direction =(player.transform.position-transform.position).normalized;
+        Vector2 targetPosition=(Vector2)transform.position+direction*moveSpeed*Time.deltaTime;
+        transform.position=targetPosition;
+    }
+    
+
+}
